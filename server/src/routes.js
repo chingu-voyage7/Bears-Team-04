bf877138042271740d6a36ae10fa16fd0e78e265
jidemobell <jidd32@gmail.com>
2018-12-09 17:45:58 +0100

    add create user test route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,24 @@
+const { createUser } = require('./controllers/users');
+
 module.exports = (app) => {
   app.get('/', (req, res) => {
     res.send({
       message: 'Hello world !'
     })
+  })
+
+  app.get('/register', (req, res) => {
+    let data = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    }
+     
+    createUser(data).then((user) => {
+      res.json(user)
+    })
+      .catch(err => res.send(err))
   })
 }
