101cbd6c925d64c4e60fd478d4fd0723f3ad77f9
Anass <maddah.anass@gmail.com>
2018-12-12 16:50:39 +0100

    Add fallback dbURI

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,8 +1,6 @@
-require('dotenv').config()
-
-console.log(process.env.PORT)
+// require('dotenv').config()
 
 module.exports = {
   port: process.env.PORT || 8081,
-  database: process.env.MONGODB_URI
+  database: process.env.MONGODB_URI || 'mongodb://localhost/bears04'
 }
