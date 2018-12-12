da6dbb4d858b1dcd9499264ec6c3d9ab560be820
Anass <maddah.anass@gmail.com>
2018-12-12 17:54:20 +0100

    Decouple register endpoint controller from route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,21 +1,23 @@
 const router = require('express').Router()
+const AuthenticationController = require('../controllers/AuthenticationController')
+
 const { body, validationResult } = require('express-validator/check')
-const User = require('../models/User')
 
 const registerCheck = [
   body('email').exists().isEmail().normalizeEmail(),
   body('password').isLength({ min: 8 })
 ]
-router.post('/register', registerCheck, (req, res) => {
-  let errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() })
-  }
 
-  User.create(req.body).then((user) => {
-    res.json(user)
-  })
-    .catch(err => res.send(err))
-})
+router.post('/register', registerCheck,
+  function validationHandler (req, res, next) {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    } else {
+      next()
+    }
+  },
+  AuthenticationController.register
+)
 
 module.exports = router
