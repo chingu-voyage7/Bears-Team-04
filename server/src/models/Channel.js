07946a0b357903116cb1e39fc2a238b1363455f3
jidemobell <jidd32@gmail.com>
2019-01-18 13:44:49 +0100

    remove admin from channel schema

--- server/src/models/Channel.js
+++ server/src/models/Channel.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema
 const ChannelSchema = mongoose.Schema(
   {
     name: { type: String, required: true, index: { unique: true } },
-    admin: { type: Schema.Types.ObjectId, ref: 'User' },
     members: [{ type: Schema.Types.ObjectId, ref: 'User' }]
   }
 )
